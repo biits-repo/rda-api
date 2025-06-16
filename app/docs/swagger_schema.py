87df66977097e2e18471911d1a21f66c722e664c
Mohammad Adeeb <mohammad.adeeb@biits.in>
2025-06-16 16:15:35 +0530

    Feat: adds api to upload the zip/pdf files

--- app/docs/swagger_schema.py
+++ app/docs/swagger_schema.py
@@ -1,6 +1,10 @@
-from drf_spectacular.utils import extend_schema
-from api.v1.serializers import FileCheckSerializer
-
+from drf_spectacular.utils import extend_schema , OpenApiParameter , OpenApiExample
+from api.v1.serializers import FileCheckSerializer , FileUploadSerializer
+from rest_framework.parsers import MultiPartParser , FormParser
+from drf_spectacular.openapi import OpenApiTypes
+from drf_spectacular.views import SpectacularAPIView
+from django.conf import settings
+from django.http import HttpResponseForbidden
 
 download_pdf_schema = extend_schema(
     operation_id="DownloadPdf",
@@ -17,3 +21,25 @@ download_audio_schema = extend_schema(
     description="Download the audio file by ISBN number",
     tags=["Media Access"],
 )
+
+
+upload_zipfile_schema = extend_schema(
+    operation_id="UploadZipfile",
+    #request=FileUploadSerializer,
+    request={
+        'multipart/form-data': {
+            'type': 'object',
+            'properties': {
+                'file': {
+                    'type': 'string',
+                    'format': 'binary'
+                },
+            }
+        },
+    },
+    responses={200: None},
+    description="Upload the zip file",
+    tags=["Media Access"],
+)
+
+
